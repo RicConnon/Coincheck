b1178f3a23566b415cd2a4b3bc36f83677be455f
Richard <rconnon@tcd.ie>
2017-12-08 13:12:54 +0000

    Adding JSP page + Controller.

--- CryptNow/CryptNow/src/main/java/com/coinchecker/controller/Test.java
+++ CryptNow/CryptNow/src/main/java/com/coinchecker/controller/Test.java
@@ -1,5 +1,19 @@
 package com.coinchecker.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class Test {
 
+	@RequestMapping(value ="/greeting")
+	public String sayHello(Model model){
+
+		model.addAttribute("greeting", "Hello World");
+		
+		//Return tie to JSP page
+		return "helloWorld";
+	}
+	
 }
